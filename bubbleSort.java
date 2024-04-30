5577085e17f4f90bae9729901580509c3ed84df0
Istear <istearahmed@gmail.com>
2024-04-30 17:25:37 +0600

    "Bubble Sort"

--- bubbleSort.java
+++ bubbleSort.java
@@ -1,1 +1,20 @@
+public class bubbleSort {
+    public static void main (String [] args){
+    int nums [] = {12,8,2,9,4,7,1};
+    int temp = 0;
 
+    for(int i=0; i<nums.length;i++){
+        for(int j=0; j<nums.length-1;j++){
+            if (nums[j]>nums[j+1]){
+                temp = nums [j+1];
+                nums[j+1]= nums[j];
+                nums[j]=temp;
+            }
+        }
+    }
+
+    for(int num:nums){
+        System.out.print(num+" ");
+    }
+}
+}
